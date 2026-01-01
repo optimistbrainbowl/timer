a4794dd5a6ca48099f3fdd229b4fa8322a5a3aa1
fourfactorial <silas.mitchell.120@gmail.com>
2026-01-01 02:45:56 -0500

    Update PWA to add caching

--- serviceworker.js
+++ serviceworker.js
@@ -1,18 +1,73 @@
+// Current version
+// Increment number whenever files change to refresh cache!
+const VERSION = "0.1";
+
+// On install, caches pages and files
+self.addEventListener("install", (e) => {
+    console.log("Service worker installed");
+    e.waitUntil(
+        caches.open(VERSION)
+        .then((cache) => cache.addAll(URLs),),
+    );
+    console.log("App files installed to cache.")
+});
+
+// Puts a request/response pair into the current version's cache
+const putInCache = async (request, response) => {
+    const cache = await caches.open(VERSION);
+    await cache.put(request,response);
+}
+
+// Default fallback: main timer
+const fallback = "/index.html";
+
+// Processes a request, using fallbackURL if it can't fetch it
+const cacheFirst = async ({request, fallbackURL}) => {
+    // First, attempt to get from cache
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
+    // Otherwise, attempt to get over network from fallback URL
+    try {
+        const responseFromNetwork = await fetch(request);
+        // If we got something, clone it
+        // Cache the clone and return the original
+        putInCache(request, responseFromNetwork.clone());
+        return responseFromNetwork;
+    } catch (error) {
+        const fallbackResponse = await caches.match(fallbackURL);
+        // If the fallback is found, return that
+        if (fallbackResponse) {
+            return fallbackResponse;
+        }
+        // Otherwise, error 404
+        return new Response("Network error happened", {
+            status: 408,
+            headers: {"Content-Type": "text/plain"}
+        });
+    }
+}
+
+// Handles fetch requests
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        cacheFirst({
+            request: event.request,
+            fallbackURL: fallback,
+        }),
+    );
+});
+
 // Change this to your repository name
 var GHPATH = '/timer';
 
 // Choose a different app prefix name
 var APP_PREFIX = 'bbtimer_';
 
-// The version of the cache. Every time you change any of the files
-// you need to change this version (version_01, version_02…). 
-// If you don't change the version, the service worker will give your
-// users the old files!
-var VERSION = 'version_00';
-
 // The files to make available for offline use. make sure to add 
 // others to this list
-var URLS = [    
+var URLs = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
   `${GHPATH}/help.html`,
